e47833da79dc82114d1f531087ba76ea47b558eb
kaisarokne <kaisa.rokne@gmail.com>
2018-01-13 19:55:08 +0100

    jeg suger og kan ingenting.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! I have no clue what I'm doing.");
+        printer(arrayTest());
+
     }
+
+    public static int[] arrayTest(){
+        int[] array = new int[6];
+        for (int x = 0; x < array.length; x++ ) {
+            array[x] = x;
+        }
+        return array;
+
+    }
+
+   public static void printer(int[] array) {
+       for (int x : array) {
+           System.out.println(x);
+       }
+   }
 }
